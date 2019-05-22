801c69755103daf1fece23b075a0ec3cd434a608
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2019-05-22 14:14:52 -0300

    Bootstrap basic structure to wrap the components are inserted by CDN

--- fluence-components/src/App.js
+++ fluence-components/src/App.js
@@ -7,17 +7,13 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+         <section className="main-renderer">
+          <div className="container row">
+            <div className="col-4">col-4</div>
+            <div className="col-4">col-4</div>
+            <div className="col-4">col-4</div>
+          </div>
+         </section>
       </header>
     </div>
   );
