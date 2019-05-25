4c1aee09a049235839c5b67c5cd1b8d0497c4bfd
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2019-05-25 03:42:04 -0300

    Style are almost done

--- fluence-components/src/App.js
+++ fluence-components/src/App.js
@@ -1,23 +1,20 @@
 import React from 'react';
-import { Theme as UWPThemeProvider, getTheme } from "react-uwp/Theme";
-import MyComponent from "./Components/MyComponent";
-import './css/App.css';
+import { Theme, getTheme } from "react-uwp/Theme";
+import CallToActionBtn from "./Components/CallToActionBtn";
+import './Assets/css/App.css';
 
 function App() {
   return (
-    <React.Fragment>
-      <UWPThemeProvider
-        className="App"
-        theme={getTheme({
-          themeName: "dark", // set custom theme
-          accent: "#0078D7", // set accent color
-          useFluentDesign: true, // sure you want use new fluent design.
-          desktopBackgroundImage: "http://127.0.0.1:8092/static/images/jennifer-bailey-10753.jpg" // set global desktop background image
-        })}
-      >
-        <MyComponent />
-      </UWPThemeProvider>
-    </React.Fragment>
+    <Theme
+      className="App"
+      theme={getTheme({
+        themeName: "dark", // set custom theme
+        accent: "#0078D7", // set accent color
+        useFluentDesign: true // sure you want use new fluent design.
+      })}
+    >
+      <CallToActionBtn />
+    </Theme>
   );
 }
 
--- fluence-components/src/Components/CallToActionBtn.js
+++ fluence-components/src/Components/CallToActionBtn.js
@@ -1,7 +1,7 @@
 import * as React from "react";
 import {Button} from "react-uwp/Button";
 
-export default class MyComponent extends React.Component{
+export default class CallToActionBtn extends React.Component{
     render(){
         return (
             <Button tooltip="Mini Tooltip">Testa Codigo</Button>
